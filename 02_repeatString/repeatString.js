db5baaae9e84a9001b3a49afbce7d9c5ccc530b2
munna-68 <mahamudmunna68@gmail.com>
2024-12-16 20:43:29 +0600

    solving the repeatString problem

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, repeatTime) {
+  let repeatedString = '';
+  for(let i = 0; i < repeatTime; i++) {
+    repeatedString += str;
+  }
+  return repeatedString;
 };
 
 // Do not edit below this line
