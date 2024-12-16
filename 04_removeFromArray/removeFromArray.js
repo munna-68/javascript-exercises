135629f7e7088ef6d3ad3d571281fd84795b0d6c
munna-68 <mahamudmunna68@gmail.com>
2024-12-16 20:57:36 +0600

    solving the removeFromArray problem
    kinda different from the solution but thats what i come up with in the first try and it works so yeah

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toRemove) {
+  let toRemoveArr = [...toRemove];
+  let removedArr = [];
+
+  for(let item of arr) {
+    for(let toRemoveItem of toRemoveArr) {
+      if(toRemoveItem === item) {
+        continue
+      } else {
+        removedArr.push(item)
+      }
+    }
+  }
+
+  return removedArr;
 };
 
 // Do not edit below this line
