d8d87c6057e0e5d264eb25333382f5b9182c9022
munna-68 <mahamudmunna68@gmail.com>
2024-12-16 21:15:14 +0600

    solving the sumAll problem
    first try again

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+  let allTheNum = [];
+  for(let i = min; i <= max; i++) {
+    allTheNum.push(i);
+  };
+  return allTheNum.reduce((acc, current) => acc + current);
 };
 
 // Do not edit below this line
