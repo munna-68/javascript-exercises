fb9b4f89d3fc4db2f051fca2cb09cfa49593df9c
munna-68 <mahamudmunna68@gmail.com>
2025-02-09 23:59:38 +0600

    solving the Calculator problem in the JS exersices

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;	
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length === 0) return 0;
+  return arr.reduce((acc, currentValue) => acc + currentValue);
 };
 
-const multiply = function() {
 
+const multiply = function(arr) {
+  return arr.reduce((acc, currentValue) => acc * currentValue);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if((num === 0 || num === 1)) return 1;
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
