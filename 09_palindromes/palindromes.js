730e965ebd3a0936a5f7cc0205783713c6c02a7c
munna-68 <mahamudmunna68@gmail.com>
2025-02-11 00:49:01 +0600

    halfway to solve the Palindrome problem

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let reversedStr = str.split('').reverse().join('')
 
+  if(str === reversedStr) return true;
+  return false
 };
 
 // Do not edit below this line
