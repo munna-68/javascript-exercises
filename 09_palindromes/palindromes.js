53d444561984c5c793da163fae6645b6384f37c4
munna-68 <mahamudmunna68@gmail.com>
2025-02-11 19:24:46 +0600

    kinda solved it but not proud of it
    solving the palindromes problem alomost all of the test were passed but not the last one cant even understanding whats is asking me to do

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,9 +1,20 @@
 const palindromes = function (str) {
-  let reversedStr = str.split('').reverse().join('')
+  let filteredArr = [];
+  let strArr = str.split('');
 
-  if(str === reversedStr) return true;
-  return false
+  // Filter out non-alphabetic characters
+  for (let word of strArr) {
+    if (/^[a-zA-Z]+$/.test(word)) {
+      filteredArr.push(word.toLowerCase()); // convert to lower case for case-insensitive check
+    }
+  }
+
+  let reversedStr = filteredArr.slice().reverse().join(''); // Create reversed version of filtered string
+
+  if (filteredArr.join('') === reversedStr) return true; // Check if the filtered string is the same forward and backward
+  return false;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
