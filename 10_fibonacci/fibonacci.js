4c018a04edcec53850e0f43e5a23e62dd79420a5
munna-68 <mahamudmunna68@gmail.com>
2025-02-12 23:11:09 +0600

    solving the fibonacci problem

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  let num = Number(n);
+  if(num < 0) return "OOPS"
+  if(num === 0) return 0;
+  if(num === 1) return 1;
+  let a = 0, b = 1, next;
+  for(let i = 2; i <= num; i++) {
+    next = a + b;
+    a = b;
+    b = next;
+  }
+  return b
 };
 
 // Do not edit below this line
