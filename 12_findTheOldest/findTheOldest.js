3d3c10afbcf91178da54e923426c4b2c3aa8809a
munna-68 <mahamudmunna68@gmail.com>
2025-02-13 03:02:07 +0600

    solved the 11th and the last problemcalled findTheOlderst and not proud of it cause i needed alot of help

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+  let oldestPerson = null;
+  let oldestAge = 0;
+  const currentYear = new Date().getFullYear(); // Get the current year
+  
+  arr.forEach(element => {
+    const yearOfDeath = element.yearOfDeath || currentYear; // If yearOfDeath is undefined, use the current year
+    const currentAge = yearOfDeath - element.yearOfBirth; // Calculate age
+    
+    if(currentAge > oldestAge) {
+      oldestAge = currentAge;
+      oldestPerson = element; // Save the whole person object
+    }
+  });
+  
+  return oldestPerson; // Return the full person object, not just the name
 };
 
 // Do not edit below this line
